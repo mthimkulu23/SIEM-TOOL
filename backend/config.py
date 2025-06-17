d6c3070a676a81508326255fe20fac438bbc32d9
mthimkulu23 <146940556+mthimkulu23@users.noreply.github.com>
2025-06-17 13:41:16 +0200

    added database

--- backend/config.py
+++ backend/config.py
@@ -6,23 +6,19 @@ class Config:
     These values would typically be loaded from environment variables in a production setup.
     """
     # MongoDB Database Configuration
-    MONGO_URI = "mongodb://localhost:27017/siem_database"
+    MONGO_URI = "mongodb+srv://thabang23mthimkulu:66F13mOoZCZAQ2O8@cluster0.wam26ka.mongodb.net/siem_database"
     DB_NAME = "siem_database"
     LOGS_COLLECTION_NAME = "logs"
     ALERTS_COLLECTION_NAME = "alerts"
 
     # Anomaly Detection Configuration
-    # Number of failed login attempts within the time window to trigger an alert
     FAILED_LOGIN_THRESHOLD = 3
-    # Time window (in seconds) for the failed login attempts to be considered a burst
     FAILED_LOGIN_TIME_WINDOW_SECONDS = 60
 
     # API Configuration
-    API_HOST = "127.0.0.1"
+    API_HOST = "0.0.0.0" # CHANGED THIS LINE
     API_PORT = 5000
 
     # Log Ingestion Configuration (conceptual)
-    # Example: Port for a Syslog listener
     SYSLOG_LISTENER_PORT = 514
-    # Kafka broker address (if using Kafka for ingestion)
     KAFKA_BROKER = "localhost:9092"
